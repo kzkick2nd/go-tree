5c38be6c3ef08e57bdb60c198ed17b414a917148
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-14 22:34:35 +0900

    Add test plan

---
+++ README.md
@@ -0,0 +1,10 @@
+## tree of Go
+
+## TODO
+- Create plan.
+- Write test.
+- Write makefile.
+- Add feature?
+
+## NOTE: Planning
+- write test
---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestStdIn(t *testing.T) {
+	// Parsing StdIn test.
+}
+
+func TestCmd(t *testing.T) {
+	// Unit test.
+}
+
+func TestSearch(t *testing.T) {
+	// Test walk and find path function.
+	// TODO: Create sample directory and files before testing.
+	// TODO: Add option -L (length).
+}
+
+func TestStyle(t *testing.T) {
+	// Test styling output from array.
+	// TODO: Add option to set color output.
+}
