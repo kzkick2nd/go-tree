24c05577386e8bf09f8d5f6e26910f6cc0e10d09
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-15 13:10:27 +0900

    Add create temp dir befor testing

--- main_test.go
+++ main_test.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	// Create test dir
+	tmpDir, err := prepareTestDirTree([]string{"test-dir/dir1", "test-dir/file1"})
+	if err != nil {
+		fmt.Printf("unable to create test dir tree: %v\n", err)
+		return
+	}
+	defer os.RemoveAll(tmpDir)
+	os.Chdir(tmpDir)
+
+	code := m.Run()
+
+	os.Exit(code)
+}
 
 func TestStdIn(t *testing.T) {
 	// Parsing StdIn test.
@@ -11,7 +30,7 @@ func TestStdIn(t *testing.T) {
 
 func TestRun(t *testing.T) {
 	// Unit test.
-	// TODO: Create sample directory and files before testing.
+	// OK: Create sample directory and files before testing.
 	cases := map[string]struct {
 		stdin  string
 		expect string
@@ -31,8 +50,8 @@ func TestRun(t *testing.T) {
 
 func TestSeek(t *testing.T) {
 	// Test walk and find path function.
-	// type Tree(with leaf) type required.
-	// TODO: Create sample directory and files before testing.
+	// OK: type Tree(with leaf) type required.
+	// OK: Create sample directory and files before testing.
 	// TODO: Add option -L (length).
 
 	cases := map[string]struct {
@@ -41,6 +60,13 @@ func TestSeek(t *testing.T) {
 	}{
 		"1": {
 			src: "./test-dir",
+			expect: tree{v: "./test-dir", n: []tree{
+				tree{v: "dir1"},
+				tree{v: "file1"},
+			}},
+		},
+		"2": {
+			src: "test-dir",
 			expect: tree{v: "test-dir", n: []tree{
 				tree{v: "dir1"},
 				tree{v: "file1"},
@@ -82,4 +108,20 @@ func TestStyle(t *testing.T) {
 			}
 		})
 	}
+}
+func prepareTestDirTree(tree []string) (string, error) {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return "", fmt.Errorf("Error creating temp directory: %v", err)
+	}
+
+	for _, t := range tree {
+		err = os.MkdirAll(filepath.Join(tmpDir, t), 0755)
+		if err != nil {
+			os.RemoveAll(tmpDir)
+			return "", err
+		}
+	}
+
+	return tmpDir, nil
 }
