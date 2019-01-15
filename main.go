908d2f6338e35a84fe722a9e6fa15505ecf0afc1
kzkick2nd <aruga.kazuki@gmail.com>
2019-01-15 13:10:44 +0900

    Success to Pass the test seek()

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
 
 func main() {
 	var p string
@@ -20,7 +25,25 @@ type tree struct {
 }
 
 func seek(src string) tree {
-	var t tree
+	t := lsr(src, src)
+	return t
+}
+
+func lsr(name, src string) tree {
+	t := tree{v: name}
+
+	list, err := ioutil.ReadDir(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range list {
+		if f.IsDir() {
+			t.n = append(t.n, lsr(f.Name(), filepath.Join(src, f.Name())))
+			continue
+		}
+		t.n = append(t.n, tree{v: f.Name(), n: []tree{}})
+	}
 	return t
 }
 
